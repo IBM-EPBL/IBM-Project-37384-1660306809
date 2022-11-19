3d48a0f9003c928e4713f36d4754c38c1e778b86
SanoshBebo <sanoshbebo@gmail.com>
2022-11-19 23:21:17 +0530

    completed

--- Project Development Phase/Sprint 4/app.py
+++ Project Development Phase/Sprint 4/app.py
@@ -1,45 +1,42 @@
-import pandas as pd
-from flask import Flask, request, jsonify, render_template,redirect,url_for
-import pickle
-
-app = Flask(__name__,template_folder='Template')
-model = pickle.load(open('D:/IBM_UAEP/model.pkl', 'rb'))
+from flask import Flask, render_template, request
+import requests
 
 
+API_KEY = "QTGabxzpqAyJDRG4AG1fjkymj3xr54Xz6-bMMK0v9WGt"
+token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey": API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+mltoken = token_response.json()["access_token"]
+header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
 
+app = Flask(__name__, static_url_path='')
 @app.route('/')
-def home():
-	return render_template('index.html')
+def index():
+    return render_template('index.html')
 
-@app.route('/predict', methods=['GET','post'])
+@app.route('/checkEligibility')
+def checkEligibility():
+    return render_template('Demo2.html')
+
+@app.route('/predict', methods=['POST'])
 def predict():
-	
-	GRE_Score = int(request.form['GRE Score'])
-	TOEFL_Score = int(request.form['TOEFL Score'])
-	University_Rating = int(request.form['University Rating'])
-	SOP = float(request.form['SOP'])
-	LOR = float(request.form['LOR'])
-	CGPA = float(request.form['CGPA'])
-	Research = int(request.form['Research'])
-	
-	final_features = pd.DataFrame([[GRE_Score, TOEFL_Score, University_Rating, SOP, LOR, CGPA,Research]])
-	predict=model.predict(final_features)
-	
-	output=predict[0]
-	if output > 0.5:
-		return redirect(url_for('chance', percent=output*100))	
-	else:
-		return redirect(url_for('no_chance', percent=output*100)) 
-	
+    greScore = int(request.form['greScore'])
+    toeflScore = int(request.form['toeflScore'])
+    univRank = int(request.form['univRank'])
+    sop = float(request.form['sop'])
+    lor = float(request.form['lor'])
+    cgpa = float(request.form['cgpa'])
+    research = int(request.form['research'])
+    array_of_input_fields = ['greScore', 'toeflScore', 'univRank', 'sop', 'lor', 'cgpa', 'research']
+    array_of_values_to_be_scored = [greScore, toeflScore, univRank, sop, lor, cgpa, research]
+    payload_scoring = {"input_data": [{"fields": [array_of_input_fields], "values": [array_of_values_to_be_scored]}]}
+    response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/250158a7-805d-4e74-aecf-920a293e52e7/predictions?version=2022-11-18', json=payload_scoring, headers={'Authorization': 'Bearer ' + mltoken})
+    predictions = response_scoring.json()
+    prediction = predictions['predictions'][0]['values'][0][0]
     
-	
-@app.route("/chance/<percent>")
-def chance(percent):
-    return render_template("chance.html", content=[percent])
+    if prediction:
+        return render_template('chance.html')
+    else:
+        return render_template('noChance.html')
 
-@app.route("/nochance/<percent>")
-def no_chance(percent):
-    return render_template("noChance.html", content=[percent])	
-	
+
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run()
